7c6a2c579747652b3ad53bec8cde4f3570a7aa06
Maged Helmy <maged.helmy1@outlook.com>
2019-08-28 11:35:59 +0200

    Updated the views and settings to allow search functionality in the backend

--- myApp/api/views.py
+++ myApp/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from myApp.models import Mlab
 from .serializers import MlabSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class MlabListView(ListAPIView):
     queryset = Mlab.objects.all()
     serializer_class = MlabSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('unique_id', 'mLab_name', 'mLab_city_location', 'mLab_department', 'clinical_condition',)
 
 
 class MlabDetailView(RetrieveAPIView):
